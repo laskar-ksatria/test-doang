c3ed36875678fb84fa3c6d55dfc0ff009e733a8d
laskar-ksatria <dock_fest@yahoo.co.id>
2020-09-08 23:55:19 +0700

    tambah

--- app.js
+++ app.js
@@ -1,39 +1,19 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT;
-const http = require('http');
-const server = http.createServer(app);
+const cookieParser = require('cookie-parser');
 
-app.use(cors({ credentials: true, origin: ['http://localhost:3000', 'http://localhost:3001'] }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 
-app.get('/gettoken', (req,res,next) => {
-    console.log('Masuk token');
-    let token = "test-token";
-    res.cookie('testcookie', token, {secure: false});
-    res.status(200).json({message: "Cookie has been set"})
-});
+app.get('/', (req, res) => res.status(200).json({message: "We are connected"}))
 
-app.get("/checktoken", (req,res,next) => {
-    let token = req.cookies.testcookie;
-    console.log(token, "TOken --------------");
-    if (token) {
-        res.status(200).json({message: "Cookie isValid"})
-    }else {
-        res.status(500).json({message: "Cookie not valid"});
-    }
-});
+app.use(require('./routes'));
 
-app.get('/cleartoken',  (req,res,next) => {
-    res.clearCookie('testcookie');
-    res.status(200).json({message: "Cookie has been clear"})
-})
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 
-server.listen(PORT, () => console.log(`Server started on ${PORT}`));
---
+++ routes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const Router = express.Router();
+const Recaptcha = require('recaptcha-v2').Recaptcha;
+
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const captchaValidate = (req,res,next) => {
+    var data = {
+		remoteip:  req.connection.remoteAddress,
+		response:  req.body.recaptcha,
+		secret: SECRET_KEY
+    }
+    var recaptcha = new Recaptcha(PUBLIC_KEY, SECRET_KEY, data);
+
+    recaptcha.verify((success, err) => {
+        if (success) {
+            console.log("Captcha success")
+            next();
+        }else {
+            res.status(400).json({message: "Invalid Captcha"})
+        }
+    })
+
+};
+
+const cookieCheck = (req,res,next) => {
+    let token = req.cookies.aloha
+    if (token) {
+        res.status(200).json({token: token});
+    }else {
+        res.status(400).json({message: "Cookie is not valid"})
+    }
+}
+
+Router.post('/login', captchaValidate,(req,res,next) => {
+    res.cookie('aloha', "owl-king");
+    res.status(200).json({message: "Cookie has been set"});
+});
+
+Router.get('/clearcookie', (req,res,next) => {
+    res.clearCookie('aloha');
+    res.status(200).json({message: "Cookie has been clear"})
+});
+
+Router.get('/seecookie', cookieCheck)
+
+module.exports = Router;
