8d3a6522e981b64805f995c97dc5962f0e2c162f
laskar-ksatria <dock_fest@yahoo.co.id>
2020-09-09 01:21:30 +0700

    samesite

--- routes.js
+++ routes.js
@@ -41,7 +41,7 @@ const cookieCheck = (req,res,next) => {
 }
 
 Router.post('/login', captchaValidate,(req,res,next) => {
-    res.cookie('aloha', "owl-king", { httpOnly: true, secure:false, maxAge: 1000, sameSite: "strict" });
+    res.cookie('aloha', "owl-king", { httpOnly: true, maxAge: 1000, sameSite: "none", });
     // res.setHeader("set-cookie", ["fromserver=1"])
     res.status(200).json({message: "Cookie has been set"});
 });
