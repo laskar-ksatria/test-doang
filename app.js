b13875f04bd511bfb87ce01631ecdc5c0fa51b95
laskar-ksatria <dock_fest@yahoo.co.id>
2020-09-09 14:46:41 +0700

    tambah

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const PORT = process.env.PORT;
 const cookieParser = require('cookie-parser');
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: ["http://localhost:3000", "http://localhost:3001"] }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
--- routes.js
+++ routes.js
@@ -40,7 +40,7 @@ const cookieCheck = (req,res,next) => {
 }
 
 Router.post('/login', captchaValidate,(req,res,next) => {
-    res.cookie('aloha', "owl-king", {});
+    res.cookie('aloha', "owl-king", {domain: 'localhost',path: '/', httpOnly: true});
     // res.setHeader("set-cookie", ["fromserver=1"])
     res.status(200).json({message: "Cookie has been set"});
 });
